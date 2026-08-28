5bb30d021df1dd0e50e44c0ea492aaec969eb710
Josh Hug <hug@cs.berkeley.edu>
2026-08-28 10:11:54 -0700

    Update HW2 starter files to use void main() and IO.println

--- hw02/src/PrintIndexed.java
+++ hw02/src/PrintIndexed.java
@@ -1,12 +1,14 @@
-/**
- * Prints each character of a given string followed by the reverse of its index.
- * Example: printIndexed("hello") -> h4e3l2l1o0
- */
-void printIndexed(String s) {
-    // TODO: Fill in this function
-}
+public class PrintIndexed {
+    /**
+     * Prints each character of a given string followed by the reverse of its index.
+     * Example: printIndexed("hello") -> h4e3l2l1o0
+     */
+    public static void printIndexed(String s) {
+        // TODO: Fill in this function
+    }
 
-void main() {
-    printIndexed("hello");
-    printIndexed("cat"); // should print c2a1t0
+    void main() {
+        printIndexed("hello");
+        printIndexed("cat"); // should print c2a1t0
+    }
 }
